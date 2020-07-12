4ebce2e6be90f3f949aaeaccc6dd66305b5f1abe
EricRicketts <eric_ricketts@me.com>
2020-07-12 19:23:23 -0400

    E5 Node Event Emitter Part Two Actual NodeJS Event Emitter update event types

--- UdemyLearnAndUnderstandNodeJS/Section5EventsAndTheEventEmitter/test/e5_node_even_emitter_part_two.test.js
+++ UdemyLearnAndUnderstandNodeJS/Section5EventsAndTheEventEmitter/test/e5_node_even_emitter_part_two.test.js
@@ -2,25 +2,27 @@ import { EventEmitter } from 'events';
 import { first, second, third, fourth } from "../files/Miscellaneous/four_functions";
 
 describe('E5 Node Event Emitter Part Two Actual NodeJS Version', function () {
-  let typeOne, typeTwo, emitter, results, expected;
+  let typeOne, typeTwo, emitter, results, expected, eventTypes;
   beforeEach(() => {
-    typeOne = 'firstGreet';
-    typeTwo = 'secondGreet';
+    eventTypes = {
+      one: 'firstGreet',
+      two: 'secondGreet'
+    }
     emitter = new EventEmitter();
-    emitter.on(typeOne, first);
-    emitter.on(typeOne, second);
-    emitter.on(typeTwo, third);
-    emitter.on(typeTwo, fourth);
+    emitter.on(eventTypes.one, first);
+    emitter.on(eventTypes.one, second);
+    emitter.on(eventTypes.two, third);
+    emitter.on(eventTypes.two, fourth);
   });
 
   it('emit type one events for Node', function () {
-    results = emitter.listeners(typeOne).map((listener) => listener());
+    results = emitter.listeners(eventTypes.one).map((listener) => listener());
     expected = ['first function', 'second function'];
     expect(results).toEqual(expected);
   });
 
   it('emit type two events for Node', function () {
-    results = emitter.listeners(typeTwo).map((listener) => listener());
+    results = emitter.listeners(eventTypes.two).map((listener) => listener());
     expected = ['third function', 'fourth function'];
     expect(results).toEqual(expected);
   });
