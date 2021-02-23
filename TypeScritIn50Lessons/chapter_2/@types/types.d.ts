b3db702ed1888021517247649e8489ac5d320e4b
EricRicketts <eric_ricketts@me.com>
2021-02-23 15:50:15 -0500

    Chapter 2 Lesson 12

--- TypeScritIn50Lessons/chapter_2/@types/types.d.ts
+++ TypeScritIn50Lessons/chapter_2/@types/types.d.ts
@@ -2,8 +2,8 @@ export type Article = {
   title: string,
   price: number,
   vat: number,
-  stock: number,
-  description: string
+  stock?: number,
+  description?: string
 }
 
 export type ShopItem = {
@@ -13,4 +13,22 @@ export type ShopItem = {
   stock: number,
   description: string,
   rating: number
+}
+
+export type Address = {
+  city: string,
+  zip: string,
+  street: string,
+  number: string
+}
+
+export type Customer = {
+  name: string,
+  address: Address,
+  dateOfBirth: date
+}
+
+export type DefaultOrder = {
+  articles: Article[],
+  customer: Customer
 }
