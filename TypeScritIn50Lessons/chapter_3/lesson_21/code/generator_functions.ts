52c8cc2d9dbca6711ece3214c032f159c3f6befd
EricRicketts <eric_ricketts@me.com>
2021-03-06 20:04:44 -0500

    chapter 3 lesson 21 Typescript in 50 lessons

---
+++ TypeScritIn50Lessons/chapter_3/lesson_21/code/generator_functions.ts
@@ -0,0 +1,16 @@
+function *generateStuff() {
+  yield 1;
+  yield 2;
+  let proceed = yield 3;
+  if (proceed) {
+    yield 4;
+  }
+  return 'done';
+}
+
+const generator = generateStuff();
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next().value);
+console.log(generator.next(true).value);
+console.log(generator.next().value);
